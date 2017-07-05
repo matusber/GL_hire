98831a32e8144ce227e647a86741e6df9dc51636
Marek Talpas <mtalpas@localhost.localdomain>
2017-07-05 20:58:59 +0200

    initialised project personal_banking

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+injectTapEventPlugin();
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <MuiThemeProvider>
+        <AppBar />
+      </MuiThemeProvider>
     );
   }
 }
